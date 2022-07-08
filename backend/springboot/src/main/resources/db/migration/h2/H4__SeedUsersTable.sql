42731da0c78cfbd909cf976355d0923dc87d2239
Mishin Yura <mishin.inbox@gmail.com>
2022-07-08 12:15:08 +0300

    Added project dependencies. Closes #3

---
+++ backend/springboot/src/main/resources/db/migration/h2/H4__SeedUsersTable.sql
@@ -0,0 +1,5 @@
+INSERT INTO users(username, password, role)
+VALUES('u', '$2a$12$J18i1MV9yf5rKASG.5v6gO7WPsyar3GZwgzCW7pVZA3..ikfuiMZa', 'ROLE_USER');
+
+INSERT INTO users(username, password, role)
+VALUES('a', '$2a$12$J18i1MV9yf5rKASG.5v6gO7WPsyar3GZwgzCW7pVZA3..ikfuiMZa', 'ROLE_ADMIN');
